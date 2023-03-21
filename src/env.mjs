c217f0e68d534cd9f7443f44d2b448b41685e70d
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-03-21 14:26:24 +0100

    - Removed prisma, we don't need it anymore

--- src/env.mjs
+++ src/env.mjs
@@ -24,7 +24,7 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
+  OPEN_WEATHER_API_KEY: process.env.OPEN_WEATHER_API_KEY,
   NODE_ENV: process.env.NODE_ENV,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
