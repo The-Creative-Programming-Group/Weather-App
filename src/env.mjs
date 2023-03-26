18d49cd0b0665ed683008bc516005bc2e92887b9
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-03-26 12:24:57 +0200

    .

--- src/env.mjs
+++ src/env.mjs
@@ -5,7 +5,6 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
@@ -24,7 +23,6 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
