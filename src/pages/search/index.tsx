22fef2a60674ed368dc28d95d52e92fdf4f4ced6
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-04-06 23:22:40 +0200

    - Changed the styling of the search page

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,36 +1,35 @@
 import React from "react";
-import styles from "./search.module.css";
 
 const search = () => {
   return (
     <>
-      <div id="styles-setup" className={styles.search_container}>
         <img
-          className={styles.search_item}
+            src="assets/background.png"
+            alt="background"
+            className="absolute w-full h-full -z-10 object-cover"
+        />
+      <div id="styles-setup" className="mt-24 absolute w-full flex justify-center">
+        <img
+          className="h-14 bg-[#735858] border-solid border-[#ddc3c3] border-8 border-r-0 pt-0.5 pb-0.5"
           src="assets/search.png"
           alt="search-icon"
         />
         <input
-          className={styles.search}
-          placeholder="Search for your location"
+          className="w-5/12 bg-[#735858] border-solid border-[#ddc3c3] border-8 border-l-0 pt-0.5 pb-0.5"
+          placeholder=" Search for your location"
           type="text"
         />
       </div>
       <iframe
         id="map"
-        className={styles.map}
         src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d5125429.316419938!2d10.415039000000002!3d51.151785999999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sde!2sde!4v1679694749290!5m2!1sde!2sde"
         width="600"
         height="450"
         allowFullScreen={false}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
+        className="absolute mt-12 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-3/5 h-4/6 border-solid border-[#ddc3c3] border-8"
       ></iframe>
-      <img
-        src="assets/background.png"
-        alt="background"
-        className={styles.background}
-      />
     </>
   );
 };
