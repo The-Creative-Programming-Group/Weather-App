6dd75c0e3a10166815c0fead7440f52416203dbc
Mona Lisa <GescheaftlichGamius@gmx.de>
2023-06-11 21:56:31 +0200

    Added home skeleton

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from "./style-home.module.css"
+import Layout from '~/components/Layout'
+
+function index() {
+  return (
+    <Layout>
+        <div className= {styles.contentflex}>
+            <div className= {styles.contentblock}>
+            <h1 className= {styles.headingtext} >London</h1>
+            <h1 className= {styles.temperature}>10°C</h1>
+            <h1 className= {styles.weather}>Sunny</h1>
+            </div>
+        </div>
+ 
+        <div className= {styles.daytemperature}></div>  
+        <div className= {styles.flex}>
+        <div className= {styles.weektemperature}></div>  
+        <div className= {styles.rain}></div>
+        <div className= {styles.map}></div>
+        </div>  
+    </Layout>
+  )
+}
+
+export default index
