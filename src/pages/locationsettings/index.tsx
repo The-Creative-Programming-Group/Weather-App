2ad0348755158041ebf0ed3dd4c32e6eb8d7896a
Fabius Schurig <127024349+Gamius00@users.noreply.github.com>
2023-07-29 00:51:33 +0200

    Button

--- src/pages/locationsettings/index.tsx
+++ src/pages/locationsettings/index.tsx
@@ -30,12 +30,12 @@ const index = () => {
     if (saveButtonTextRef.current == undefined) return;
     if (buttonName === "Save changes") return;
     saveButtonTextRef.current.animate(
-      {
-        opacity: [0, 1],
-      },
-      {
-        duration: 500,
-      }
+        {
+          opacity: [0, 1],
+        },
+        {
+          duration: 500,
+        }
     );
   }, [buttonName]);
 
@@ -66,12 +66,12 @@ const index = () => {
 
   const checkCity = () => {
     cities.map(
-      (stadt) => {
-        if (stadt.name.toLowerCase() === searchValue.toLowerCase()) {
-          changed(stadt.name);
-        }
-      },
-      [searchValue]
+        (stadt) => {
+          if (stadt.name.toLowerCase() === searchValue.toLowerCase()) {
+            changed(stadt.name);
+          }
+        },
+        [searchValue]
     );
   };
 
@@ -101,150 +101,150 @@ const index = () => {
   cities.sort((stadtA, stadtB) => stadtB.population - stadtA.population);
 
   return (
-    <>
-      <Layout title={"Location Settings"}>
-        <div className="flex w-full flex-col items-center">
-          <h1 className="mt-24 flex justify-center text-3xl font-bold">
-            Location settings
-          </h1>
-          <hr className="mt-3 h-1.5 w-4/12 rounded bg-[#2d3142]" />
-          <div
-            id="styles-setup"
-            className="mt-9 flex w-full flex-col items-center"
-          >
-            <label className="mr-128 mb-2">Change location</label>
-            <div className="flex w-full justify-center">
-              <img
-                className="w-12 transform border-b-2 border-black bg-[#d8d5db] pt-3 pb-3 pl-3"
-                src="assets/search2.png"
-                alt="search-icon"
-                width={56}
-                height={56}
-              />
-              <input
-                ref={firstInputRef}
-                className="w-4/12 border-b-2 border-black bg-[#d8d5db] pt-0.5 pb-0.5 pl-3 text-xl font-bold text-black outline-0"
-                placeholder="Search for your location"
-                type="text"
-                onFocus={() => handleInputFocus("input1")}
-                onChange={handleChange}
-                value={searchValue}
-                onBlur={handleInputBlur}
-              />
-            </div>
-            {cities.map((stadt) => {
-              if (
-                searchValue !== "" &&
-                stadt.name.toLowerCase().startsWith(searchValue.toLowerCase())
-              ) {
-                anzahl++;
-
-                if (anzahl <= 4) {
-                  return (
-                    <div
-                      className={
-                        activeInput !== "input1"
-                          ? "hidden"
-                          : "h-auto w-4/12+12px border-b-2 border-gray-400 bg-[#d8d5db] p-5"
-                      }
-                      key={stadt.name}
-                      onMouseDown={() => handleStadtclick(stadt.name)}
-                    >
-                      <p>
-                        {stadt.name
-                          .split("")
-                          .map((buchstabe, buchstabenIndex) => (
-                            <span
-                              className={
-                                buchstabenIndex < searchValue.length
-                                  ? "font-bold"
-                                  : ""
-                              }
-                            >
-                              {buchstabe}
-                            </span>
-                          ))}
-                      </p>
-                    </div>
-                  );
-                }
-              }
-            })}
-            <button
-              onClick={handleChangeclick}
-              onMouseDown={handleChangedown}
-              className="mt-2.5 rounded border-solid bg-[#2d3142] p-2 font-bold text-white"
-              ref={saveButtonTextRef}
-            >
-              {buttonName}
-            </button>
-            <div />
-
+      <>
+        <Layout title={"Location Settings"}>
+          <div className="flex w-full flex-col items-center">
+            <h1 className="mt-24 flex justify-center text-3xl font-bold">
+              Location settings
+            </h1>
+            <hr className="mt-3 h-1.5 w-4/12 rounded bg-[#2d3142]" />
             <div
-              id="styles-setup"
-              className="mt-9 flex w-full flex-col items-center"
+                id="styles-setup"
+                className="mt-9 flex w-full flex-col items-center"
             >
-              <label className="mr-131 mb-2">Add new location</label>
+              <label className="mr-128 mb-2">Change location</label>
               <div className="flex w-full justify-center">
                 <img
-                  className="w-12 transform border-b-2 border-black bg-[#d8d5db] pt-3 pb-3 pl-3"
-                  src="assets/search2.png"
-                  alt="search-icon"
-                  width={56}
-                  height={56}
+                    className="w-12 transform border-b-2 border-black bg-[#d8d5db] pt-3 pb-3 pl-3"
+                    src="assets/search2.png"
+                    alt="search-icon"
+                    width={56}
+                    height={56}
                 />
                 <input
-                  className="w-4/12 border-b-2 border-black bg-[#d8d5db] pt-0.5 pb-0.5 pl-3 text-xl font-bold text-black outline-0"
-                  placeholder="Search for your location"
-                  type="text"
-                  onFocus={() => handleInputFocus("input2")}
-                  value={searchValue2}
-                  onBlur={handleInputBlur}
-                  onChange={handleChange2}
+                    ref={firstInputRef}
+                    className="w-4/12 border-b-2 border-black bg-[#d8d5db] pt-0.5 pb-0.5 pl-3 text-xl font-bold text-black outline-0"
+                    placeholder="Search for your location"
+                    type="text"
+                    onFocus={() => handleInputFocus("input1")}
+                    onChange={handleChange}
+                    value={searchValue}
+                    onBlur={handleInputBlur}
                 />
-              </div>{" "}
-            </div>
-            {cities.map((stadt) => {
-              if (
-                searchValue2 !== "" &&
-                stadt.name.toLowerCase().startsWith(searchValue2.toLowerCase())
-              ) {
-                anzahl++;
+              </div>
+              {cities.map((stadt) => {
+                if (
+                    searchValue !== "" &&
+                    stadt.name.toLowerCase().startsWith(searchValue.toLowerCase())
+                ) {
+                  anzahl++;
 
-                if (anzahl <= 4) {
-                  return (
-                    <div
-                      className={
-                        activeInput !== "input2"
-                          ? "hidden"
-                          : "h-auto w-4/12+12px border-b-2 border-gray-400 bg-[#d8d5db] p-5"
-                      }
-                      key={stadt.name}
-                    >
-                      <p>
-                        {stadt.name
-                          .split("")
-                          .map((buchstabe, buchstabenIndex) => (
-                            <span
-                              className={
-                                buchstabenIndex < searchValue.length
-                                  ? "font-bold"
-                                  : ""
-                              }
-                            >
+                  if (anzahl <= 4) {
+                    return (
+                        <div
+                            className={
+                              activeInput !== "input1"
+                                  ? "hidden"
+                                  : "h-auto w-4/12+12px border-b-2 border-gray-400 bg-[#d8d5db] p-5"
+                            }
+                            key={stadt.name}
+                            onMouseDown={() => handleStadtclick(stadt.name)}
+                        >
+                          <p>
+                            {stadt.name
+                                .split("")
+                                .map((buchstabe, buchstabenIndex) => (
+                                    <span
+                                        className={
+                                          buchstabenIndex < searchValue.length
+                                              ? "font-bold"
+                                              : ""
+                                        }
+                                    >
                               {buchstabe}
                             </span>
-                          ))}
-                      </p>
-                    </div>
-                  );
+                                ))}
+                          </p>
+                        </div>
+                    );
+                  }
                 }
-              }
-            })}
+              })}
+              <button
+                  onClick={handleChangeclick}
+                  onMouseDown={handleChangedown}
+                  className="mt-2.5 rounded border-solid bg-[#2d3142] p-2 font-bold text-white"
+                  ref={saveButtonTextRef}
+              >
+                {buttonName}
+              </button>
+              <div />
+
+              <div
+                  id="styles-setup"
+                  className="mt-9 flex w-full flex-col items-center"
+              >
+                <label className="mr-131 mb-2">Add new location</label>
+                <div className="flex w-full justify-center">
+                  <img
+                      className="w-12 transform border-b-2 border-black bg-[#d8d5db] pt-3 pb-3 pl-3"
+                      src="assets/search2.png"
+                      alt="search-icon"
+                      width={56}
+                      height={56}
+                  />
+                  <input
+                      className="w-4/12 border-b-2 border-black bg-[#d8d5db] pt-0.5 pb-0.5 pl-3 text-xl font-bold text-black outline-0"
+                      placeholder="Search for your location"
+                      type="text"
+                      onFocus={() => handleInputFocus("input2")}
+                      value={searchValue2}
+                      onBlur={handleInputBlur}
+                      onChange={handleChange2}
+                  />
+                </div>{" "}
+              </div>
+              {cities.map((stadt) => {
+                if (
+                    searchValue2 !== "" &&
+                    stadt.name.toLowerCase().startsWith(searchValue2.toLowerCase())
+                ) {
+                  anzahl++;
+
+                  if (anzahl <= 4) {
+                    return (
+                        <div
+                            className={
+                              activeInput !== "input2"
+                                  ? "hidden"
+                                  : "h-auto w-4/12+12px border-b-2 border-gray-400 bg-[#d8d5db] p-5"
+                            }
+                            key={stadt.name}
+                        >
+                          <p>
+                            {stadt.name
+                                .split("")
+                                .map((buchstabe, buchstabenIndex) => (
+                                    <span
+                                        className={
+                                          buchstabenIndex < searchValue.length
+                                              ? "font-bold"
+                                              : ""
+                                        }
+                                    >
+                              {buchstabe}
+                            </span>
+                                ))}
+                          </p>
+                        </div>
+                    );
+                  }
+                }
+              })}
+            </div>
           </div>
-        </div>
-      </Layout>
-    </>
+        </Layout>
+      </>
   );
 };
 
