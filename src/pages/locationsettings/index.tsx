4413f53760182b3c2472129b7f3f88043768042b
Gamius00 <gescheaftlichgamius@gmx.de>
2023-08-03 11:32:50 +0200

    add New Location

--- src/pages/locationsettings/index.tsx
+++ src/pages/locationsettings/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef, type ChangeEvent } from "react";
 import Layout from "~/components/Layout";
-import { ToastContainer, toast } from "react-toastify";
 import { activeCity$ } from "~/states";
 import Image from "next/image";
 
@@ -10,7 +9,7 @@ const LocationSettings = () => {
     population: number;
   }
 
-  // Only for testing, later it will be fetched from an API
+  // Only for testing, later it will be fetched from an API. Now its the TestArray (data) with the cities and their population.
   const cities: ICity[] = [
     { name: "Berlin", population: 1000000 },
     { name: "Dresden", population: 510000 },
@@ -20,14 +19,19 @@ const LocationSettings = () => {
     { name: "Delb", population: 20000 },
   ];
 
-  const [searchValue, setSearchValue] = useState("");
-  const [searchValue2, setSearchValue2] = useState("");
-  const [activeInput, setActiveInput] = useState<string | null>(null);
-  const firstInputRef = useRef<HTMLInputElement>(null);
-  const [buttonName, setButtonName] = useState("Change Location");
-  const saveButtonTextRef = useRef<HTMLButtonElement>(null);
-  const [isLocationSelected, setIsLocationSelected] = useState(false);
+  const [searchValue, setSearchValue] = useState(""); // SearchValue is the value of the input field
+  const [searchValue2, setSearchValue2] = useState(""); // SearchValue2 is the value of the second input field
+  const [activeInput, setActiveInput] = useState<string | null>(null); // activeInput is the input field which is active
+  const firstInputRef = useRef<HTMLInputElement>(null); // firstInputRef is the ref of the first input field
+  const secondInputRef = useRef<HTMLInputElement>(null); // secondInputRef is the ref of the first input field
+  const [buttonName, setButtonName] = useState("Change Location"); // buttonName is the name of the first button
+  const [secondButtonName, setsecondButtonName] = useState("Add New Location"); // buttonName is the name of the second button
+  const saveButtonTextRef = useRef<HTMLButtonElement>(null); // saveButtonTextRef is the ref of the first button
+  const saveButtonTextRef2 = useRef<HTMLButtonElement>(null); // saveButtonTextRef is the ref of the second button
+  const [isLocationSelected, setIsLocationSelected] = useState(false); // isLocationSelected is true if the user selected a location (first input)
+  const [isLocation2Selected, setIsLocation2Selected] = useState(false); // isLocation2Selected is true if the user selected a location (second input)
 
+  //Animation for the changeButton
   useEffect(() => {
     if (saveButtonTextRef.current == undefined) return;
     if (buttonName === "Save changes") return;
@@ -41,23 +45,45 @@ const LocationSettings = () => {
     );
   }, [buttonName]);
 
+  //Animation for the AddButton
+  useEffect(() => {
+    if (saveButtonTextRef2.current == undefined) return;
+    if (buttonName === "Save changes") return;
+    saveButtonTextRef2.current.animate(
+      {
+        opacity: [0, 1],
+      },
+      {
+        duration: 500,
+      },
+    );
+  }, [secondButtonName]);
+
+  //Will change button name to ChangeLocation if the first input field not empty
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setButtonName("Change Location");
     setSearchValue(event.target.value);
   };
 
+
+  //Set the active input value to the input field which is active
   const handleInputFocus = (inputName: string) => {
     setActiveInput(inputName);
   };
 
+
+  //Will set no active input if the user click outside the input field
   const handleInputBlur = () => {
     setActiveInput(null);
   };
 
+  //Will set the value of the second input field to the city if the user click on a proposed city in the list
   const handleChange2 = (event: ChangeEvent<HTMLInputElement>) => {
+    setsecondButtonName("Add New Location");
     setSearchValue2(event.target.value);
   };
 
+  //Will focus the first input field if the user click on the change button
   const handleChangeclick = () => {
     if (searchValue === "") {
       if (firstInputRef.current) {
@@ -66,6 +92,15 @@ const LocationSettings = () => {
     }
   };
 
+  const handleChangeclick2 = () => {
+    if (searchValue2 === "") {
+      if (secondInputRef.current) {
+        secondInputRef.current.focus();
+      }
+    }
+  };
+
+  //Will check if the city is in the array and will change the city if it is in the array
   const checkCity = () => {
     cities.map(
       (stadt) => {
@@ -77,13 +112,37 @@ const LocationSettings = () => {
     );
   };
 
+  const checkCity2 = () => {
+    cities.map(
+      (stadt) => {
+        if (stadt.name.toLowerCase() === searchValue2.toLowerCase()) {
+          changed2(stadt.name);
+        }
+      },
+      [searchValue2],
+    );
+  };
+
+  //Will change the city
   const changed = (stadt: string) => {
     activeCity$.set(stadt);
     setButtonName("Changed");
+    setsecondButtonName("Add New Location");
     setSearchValue("");
     setIsLocationSelected(false);
   };
+  
+   //Will add the city
+  const changed2 = (stadt: string) => {
+    activeCity$.set(stadt);
+    setsecondButtonName("Added");
+    setButtonName("Change Location");
+    setSearchValue2("");
+    setIsLocation2Selected(false);
+  };
 
+
+  //Will check if the user click on the change button or if he click on the city in the list
   const handleChangedown = () => {
     if (isLocationSelected === true) {
       changed(searchValue);
@@ -92,11 +151,28 @@ const LocationSettings = () => {
     }
   };
 
+//Will check if the user click on the change button or if he click on the city in the list
+  const handleChangedown2 = () => {
+    if (isLocation2Selected === true) {
+      changed2(searchValue2);
+    } else {
+      checkCity2();
+    }
+  };
+
+  //Will set the value of the first input field to the city if the user click on a proposed city in the list
+  const handleStadtclick2 = (name: string) => {
+    setSearchValue2(name);
+    setIsLocation2Selected(true);
+  };
+
+  //Will set the value of the first input field to the city if the user click on a proposed city in the list
   const handleStadtclick = (name: string) => {
     setSearchValue(name);
     setIsLocationSelected(true);
   };
 
+  //Sort the cities by population
   let anzahl = 0;
   cities.sort((stadtA, stadtB) => stadtB.population - stadtA.population);
 
@@ -202,6 +278,7 @@ const LocationSettings = () => {
                   value={searchValue2}
                   onBlur={handleInputBlur}
                   onChange={handleChange2}
+                  ref={secondInputRef}
                 />
               </div>{" "}
             </div>
@@ -221,6 +298,7 @@ const LocationSettings = () => {
                           : "w-4/12+12px h-auto border-b-2 border-gray-400 bg-[#d8d5db] p-5"
                       }
                       key={stadt.name}
+                      onMouseDown={() => handleStadtclick2(stadt.name)}
                     >
                       <p>
                         {stadt.name
@@ -228,7 +306,7 @@ const LocationSettings = () => {
                           .map((buchstabe, buchstabenIndex) => (
                             <span
                               className={
-                                buchstabenIndex < searchValue.length
+                                buchstabenIndex < searchValue2.length
                                   ? "font-bold"
                                   : ""
                               }
@@ -243,6 +321,14 @@ const LocationSettings = () => {
                 }
               }
             })}
+                   <button
+              onClick={handleChangeclick2}
+              onMouseDown={handleChangedown2}
+              className="mt-2.5 rounded border-solid bg-[#2d3142] p-2 font-bold text-white"
+              ref={saveButtonTextRef2}
+            >
+              {secondButtonName}
+            </button>
           </div>
         </div>
       </Layout>
