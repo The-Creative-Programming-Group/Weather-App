98d6bf919594064292691a271350d8b33a43db6b
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-09-03 19:12:37 +0200

    Fixed the backend

--- src/server/api/routers/reverseGeo.ts
+++ src/server/api/routers/reverseGeo.ts
@@ -4,11 +4,13 @@ import { env } from "~/env.mjs";
 import axios from "axios";
 import { log } from "next-axiom";
 
-const reverseGeoSchema = z.object({
-  name: z.string(),
-  country: z.string(),
-  state: z.string(),
-});
+const reverseGeoSchema = z.array(
+    z.object({
+      name: z.string(),
+      country: z.string(),
+      state: z.string(),
+      })
+);
 
 type ReverseGeo = z.infer<typeof reverseGeoSchema> | undefined;
 
@@ -23,25 +25,39 @@ export const reverseGeoRouter = createTRPCRouter({
       }),
     )
     .query(async ({ input }) => {
-      log.info("User requested location data for coordiantes", {
+      log.info("User requested location data for coordinates", {
         coordinates: input.coordinates,
       });
 
       const urlReverseGeo = `https://api.api-ninjas.com/v1/reversegeocoding?lat=${input.coordinates.lat.toString()}&lon=${
-        input.coordinates.lng.toString
+        input.coordinates.lng.toString()
       }`;
-
-      let reverseGeoResult = axios.get<ReverseGeo>(urlReverseGeo, {
-        headers: {
-          "X-Api-Key": `${env.API_NINJAS_API_KEY}`,
-        },
-      });
 
       let reverseGeoData: ReverseGeo = undefined;
 
-      reverseGeoData = reverseGeoSchema.parse((await reverseGeoResult).data);
+      try {
+        let reverseGeoResult = await axios.get<ReverseGeo>(urlReverseGeo, {
+          headers: {
+            "X-Api-Key": `${env.API_NINJAS_API_KEY}`,
+          },
+        });
+        /* log.debug("Reverse geocoding data", {
+            data: (reverseGeoResult).data,
+        }); */
+        reverseGeoData = reverseGeoSchema.parse((reverseGeoResult).data);
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          log.error("Zod Errors", error.issues);
+        } else {
+          log.error("Error while fetching reverse geocoding data", {
+            error: error,
+          });
+        }
+      }
+
+
       return {
-        city: reverseGeoData?.name,
+        city: reverseGeoData && reverseGeoData[0] ? reverseGeoData[0].name : "Unknown"
       };
     }),
 });
