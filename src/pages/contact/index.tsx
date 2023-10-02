59117dd00d148ea638986446255a020dd4fbfc62
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-10-03 00:14:35 +0200

    Added translations to the contact us

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import Layout from "~/components/Layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 const ContactUs = () => {
-  type SendButtonTextType = "Send" | "Sent";
-  const [sendButtonText, setSendButtonText] =
-    React.useState<SendButtonTextType>("Send");
+  const { t: translationContact } = useTranslation("contact");
+  const { t: translationCommon } = useTranslation("common");
+
+  const [sendButtonText, setSendButtonText] = React.useState<string>(
+    translationContact("send button"),
+  );
   const sendButtonTextRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -29,48 +33,56 @@ const ContactUs = () => {
 
   return (
     <>
-      <Layout title="Contact Us">
+      <Layout title={translationCommon("menu contact")}>
         <div className="flex w-full flex-col items-center">
           <h1 className="mt-10 flex justify-center text-4xl font-bold">
-            Contact Us
+            {translationCommon("menu contact")}
           </h1>
           <hr className="mt-3 h-1.5 w-6/12 rounded bg-[#2d3142] md:w-4/12" />
           <div className="mb-2 ml-5 mr-5 mt-5 flex flex-col md:w-1/3 md:flex-row">
             <div className="mb-2 flex w-60 flex-col md:mb-0 md:mr-4 md:w-full">
-              <label className="ml-2">First name</label>
+              <label className="ml-2">{translationContact("first name")}</label>
               <input
                 className="h-10 w-full rounded-md bg-[#d7d5db] pl-2"
-                onChange={() => setSendButtonText("Send")}
+                onChange={() =>
+                  setSendButtonText(translationContact("send button"))
+                }
                 // Autofocus didn't work, so I used this instead
                 ref={inputRef}
                 title="Search"
               />
             </div>
             <div className="flex w-60 flex-col md:w-full">
-              <label className="ml-2">Last name</label>
+              <label className="ml-2">{translationContact("last name")}</label>
               <input
                 className="h-10 w-full rounded-md bg-[#d8d5db] pl-2"
-                onChange={() => setSendButtonText("Send")}
+                onChange={() =>
+                  setSendButtonText(translationContact("send button"))
+                }
               />
             </div>
           </div>
           <div className="mb-2 flex w-60 flex-col md:w-4/12">
-            <label className="ml-2">Email</label>
+            <label className="ml-2">{translationContact("email")}</label>
             <input
               className="h-10 w-full rounded-md bg-[#d8d5db] pl-2"
-              onChange={() => setSendButtonText("Send")}
+              onChange={() =>
+                setSendButtonText(translationContact("send button"))
+              }
             />
           </div>
           <div className="mb-8 flex w-60 flex-col md:w-4/12">
-            <label className="ml-2">Message</label>
+            <label className="ml-2">{translationContact("message")}</label>
             <textarea
               className="h-64 w-full resize-none rounded-md bg-[#d8d5db] pl-2"
-              onChange={() => setSendButtonText("Send")}
+              onChange={() =>
+                setSendButtonText(translationContact("send button"))
+              }
             />
           </div>
           <button
             className="h-9 w-60 rounded border-solid bg-[#2d3142] font-bold text-white"
-            onClick={() => setSendButtonText("Sent")}
+            onClick={() => setSendButtonText(translationContact("sent button"))}
           >
             <div ref={sendButtonTextRef}>{sendButtonText}</div>
           </button>
@@ -83,7 +95,7 @@ const ContactUs = () => {
 export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common"])),
+      ...(await serverSideTranslations(locale, ["contact", "common"])),
     },
   };
 }
