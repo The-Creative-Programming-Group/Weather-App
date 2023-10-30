9960815720cb25ddb451672674d4ae14c6d1b182
Jakob Rössner <jakob@lylu.de>
2023-10-30 08:19:13 +0100

    If you already chose a city you will be redirected to /home in start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,16 @@ import Image from "next/image";
 import background from "~/assets/background.png";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
+import { activeCity$ } from "~/states";
+import { useRouter } from "next/router";
 
 const PublicHome: NextPage = () => {
   const { t: translation } = useTranslation("common");
+  const router = useRouter();
+
+  if (activeCity$.id.get() !== 0 && activeCity$.name.get() !== "") {
+    router.push("/home");
+  }
 
   return (
     <>
