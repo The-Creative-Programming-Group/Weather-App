b8591b6f8fe21f8286ab0338d7bc7e3d81f66aa0
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-12-16 21:03:08 +0100

    feat: Added a costum 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Layout from "~/components/Layout";
+import { useTranslation } from "next-i18next";
+
+export default function Custom404() {
+  const { t: translation404 } = useTranslation("404");
+  const { t: translationCommon } = useTranslation("common");
+  return (
+    <Layout title={translationCommon("title 404 page")}>
+      <div className="flex w-full flex-col items-center">
+        <h1 className="mt-10 text-4xl font-bold">{translation404("title")}</h1>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["404", "common"])),
+    },
+  };
+}
