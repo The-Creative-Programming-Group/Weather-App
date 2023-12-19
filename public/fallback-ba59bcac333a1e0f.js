9439dbaea396eb45de4c7d38081f1ee09c575418
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-12-19 20:12:23 +0100

    deps: Updated some dependencies

--- public/fallback-ba59bcac333a1e0f.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";self.fallback=async e=>{let{destination:a,url:n}=e,o={document:"/_offline",image:!1,audio:!1,video:!1,font:!1}[a];return o?caches.match(o,{ignoreSearch:!0}):Response.error()}})();
