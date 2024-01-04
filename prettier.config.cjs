90e2f6f8b428362a9fd10b6569c2629d6fa4dc3a
Turbobot <turbobot@vercel.com>
2024-01-04 17:58:59 +0100

    feat(create-turbo): apply pnpm-eslint transform

--- prettier.config.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import("prettier").Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-};
-
-module.exports = config;
