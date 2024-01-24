e432174e0602c9228467cc7d5dae83f98604b653
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-01-24 19:00:22 +0100

    style: Improved the style regarding a not centered text on the 404 page

--- apps/web/src/pages/404.tsx
+++ apps/web/src/pages/404.tsx
@@ -1,6 +1,7 @@
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Layout from "~/components/Layout";
 import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import Layout from "~/components/Layout";
 
 // For more information about 404 page, please visit https://nextjs.org/docs/pages/building-your-application/routing/custom-error#404-page
 export default function Custom404() {
@@ -9,7 +10,9 @@ export default function Custom404() {
   return (
     <Layout title={translationCommon("title 404 page")}>
       <div className="flex w-full flex-col items-center">
-        <h1 className="mt-10 text-4xl font-bold">{translation404("title")}</h1>
+        <h1 className="mt-10 text-center text-4xl font-bold">
+          {translation404("title")}
+        </h1>
       </div>
     </Layout>
   );
