9eace32a12ce8411444b539ef027d2658659ccde
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-03-08 18:48:01 +0100

    feat: Added the indonesian translation

---
+++ apps/web/src/locales/id.ts
@@ -0,0 +1,189 @@
+export default {
+  // 404 page translations
+  "404.title": "404 - Halaman Tidak Ditemukan",
+
+  // common translations
+  "common.title": "Weather.io",
+  "common.location": "Indonesia",
+
+  "common.start button": "Temukan lokasimu",
+  "common.share button": "Bagikan",
+  "common.menu home": "Beranda",
+  "common.menu locations": "Lokasi",
+  "common.menu settings": "Pengaturan",
+  "common.menu contact": "Kontak",
+  "common.footer legal": "Hukum",
+  "common.footer contributors": "Kontributor",
+
+  "common.search page title": "Cari - Weather.io",
+
+  "common.miles per hour": "Mil per jam",
+  "common.kilometers per hour": "Kilometer per jam",
+  "common.knots": "Knots",
+  "common.meters per second": "Meter per detik",
+  "common.beaufort": "Beaufort",
+  "common.hectopascals": "Hectopascals",
+
+  "common.share text": "Lihat halaman ini: ",
+
+  "common.title 404 page": "404: This page could not be found",
+  "common.title offline page": "Offline",
+
+  // settings page translations
+  "settings.temperature units": "Satuan suhu",
+  "settings.wind speed units": "Satuan kecepatan angin",
+  "settings.language": "Bahasa",
+
+  "settings.english": "Inggris",
+  "settings.german": "Jerman",
+  "settings.indonesian": "Indonesia",
+
+  // locationsettings page translations
+  "locationsettings.add new location": "Tambah lokasi baru",
+  "locationsettings.add new location button": "Tambah lokasi baru",
+  "locationsettings.search input placeholder": "Cari lokasi",
+
+  "locationsettings.at least one city toast":
+    "Anda harus memilih setidaknya satu kota",
+  "locationsettings.switched to city toast": "Beralih ke kota!",
+  "locationsettings.added city toast": "Menambahkan kota baru!",
+  "locationsettings.city not found toast": "Kota tidak ditemukan!",
+  "locationsettings.try again toast": "Coba lagi!",
+
+  "locationsettings.my location button": "Pilih lokasi",
+
+  // offline page translations
+  "offline.title": "Anda sedang offline",
+  "offline.message": "Silakan periksa koneksi internet Anda dan coba lagi.",
+
+  // search page translations
+  "search.search input placeholder": "Cari lokasi",
+  "search.continue button": "Lanjutkan -->",
+
+  "search.my location button": "Pilih lokasi",
+
+  // legal page translations
+  "legal.main text":
+    "Isi dari situs web ini telah diperiksa dan dibuat dengan hati-hati. Namun, kami tidak menjamin akurasi, kelengkapan, dan ketepatan waktu informasi yang disediakan. Kami tidak bertanggung jawab atas kerusakan atau kerugian yang timbul dari penggunaan situs web ini. Para operator halaman yang terhubung bertanggung jawab sepenuhnya atas kontennya. Setiap tanggung jawab atas konten eksternal secara tegas dikecualikan.",
+  "legal.germany": "Jerman",
+  "legal.information disclaimer": "Informasi sesuai dengan § 5 TMG",
+
+  // home page translations
+  "home.today": "Hari Ini",
+  "home.9 day forecast": "Perkiraan 9 Hari",
+  "home.precipitation": "Presipitasi",
+  "home.feels like": "Terasa Seperti",
+  "home.visibility": "Visibilitas",
+  "home.wind pressure": "Angin & Tekanan",
+  "home.air quality": "Kualitas Udara",
+  "home.pressure": "Tekanan",
+  "home.speed": "Kecepatan",
+  "home.moon phase": "Fase Bulan",
+
+  "home.early morning": "Pagi Awal",
+  "home.morning": "Pagi",
+  "home.noon": "Siang",
+  "home.afternoon": "Sore",
+  "home.night": "Malam",
+
+  "home.feels like phrase extremely warm": "The sun is scorching today!",
+  "home.feels like phrase very warm":
+    "Matahari mungkin mencoba untuk mendinginkan!",
+  "home.feels like phrase warm": "Bahkan es krim pun merasa terlalu hangat!",
+  "home.feels like phrase slightly more warm": "It's a toasty day!",
+  "home.feels like phrase slightly warm": "A perfect day for a picnic!",
+  "home.feels like phrase neutral":
+    "Just the right temperature, not too hot, not too cold.",
+  "home.feels like phrase slightly cold": "A light jacket might be needed.",
+  "home.feels like phrase slightly more cold": "It's a frosty day!",
+  "home.feels like phrase cold": "Penguin memesan cokelat panas karena dingin!",
+  "home.feels like phrase very cold":
+    "Cuaca yang dingin membuat bahkan boneka salju pun menggigil!",
+  "home.feels like phrase extremely cold":
+    "It's so cold, even the polar bears are staying inside!",
+
+  "home.air quality text very poor": "Sangat Buruk",
+  "home.air quality text poor": "Buruk",
+  "home.air quality text fair": "Cukup Baik",
+  "home.air quality text moderate": "Sedang",
+  "home.air quality text good": "Baik",
+  "home.air quality text excellent": "Sangat Baik",
+
+  "home.sunset": "Matahari Terbenam",
+  "home.sunrise": "Matahari Terbit",
+
+  "home.moonset": "Moonset",
+  "home.moonrise": "Moonrise",
+
+  "home.weather state stormy": "Badai",
+  "home.weather state snowy": "Berawan",
+  "home.weather state rainy": "Hujan",
+  "home.weather state very cloudy": "Sangat Berawan",
+  "home.weather state cloudy": "Berawan",
+  "home.weather state windy": "Berangin",
+  "home.weather state sunny": "Cerah",
+
+  "home.this hour": "Jam Ini",
+  "home.early hour time ending": "Pagi",
+  "home.late hour time ending": "Malam",
+
+  "home.9 day forecast card content":
+    "Perkiraan untuk 9 hari ke depan. <br />Suhu rata-rata adalah suhu rata-rata selama hari. Satu untuk siang dan satu untuk malam.",
+
+  "home.precipitation card title": "Presipitasi Saat Ini",
+
+  "home.feels like card content":
+    "Parameter suhu ini memperhitungkan persepsi manusia terhadap cuaca",
+
+  "home.visibility card title": "Visibilitas Saat Ini",
+
+  "home.wind pressure card title": "Angin & Tekanan Saat Ini",
+  "home.wind pressure card content":
+    "<b>Atmospheric pressure on the sea level.</b> <br /> At sea level, atmospheric pressure is the force exerted by the weight of the air above, averaging around 1013.25 hPa.",
+
+  "home.not available": "Tidak Tersedia",
+
+  "home.moon phase card title": "Fase Bulan Saat Ini",
+  "home.moon phase card content":
+    "Fase bulan adalah bentuk bagian yang disinari langsung oleh Matahari dari Bulan saat dilihat dari Bumi.",
+
+  "home.moon phase new moon": "Bulan Baru",
+  "home.moon phase waxing crescent": "Sabit Membesar",
+  "home.moon phase first quarter": "Kuartal Pertama",
+  "home.moon phase waxing gibbous": "Membesar Penuh",
+  "home.moon phase full moon": "Purnama",
+  "home.moon phase waning gibbous": "Mengering Penuh",
+  "home.moon phase last quarter": "Kuartal Terakhir",
+  "home.moon phase waning crescent": "Sabitan Mengecil",
+
+  "home.more information": "Informasi Lebih Lanjut",
+  "home.less information": "Informasi Lebih Sedikit",
+
+  "home.sun hours": "Jam Matahari",
+
+  "home.from": "Dari",
+
+  // contact page translations
+  "contact.send button": "Kirim",
+  "contact.sent button": "Terkirim",
+  "contact.first name": "Nama depan",
+  "contact.last name": "Nama belakang",
+  "contact.email": "Email",
+  "contact.message": "Pesan",
+
+  "contact.sent toast": "Pesan terkirim!",
+  "contact.sending email loading toast": "Mengirim email...",
+
+  "contact.invalid email": "Alamat email tidak valid",
+  "contact.at least 2 characters": "Kolom ini harus berisi minimal 2 karakter",
+  "contact.at least 10 characters":
+    "Kolom ini harus berisi minimal 10 karakter",
+
+  // contributors page translations
+  "contributors.made with love text":
+    'Dibuat dengan <span class="text-4xl" style="color: rgb(239 68 68)">♥\uFE0F</span> by',
+  "contributors.founder": "Pendiri",
+  "contributors.engineer": "Insinyur",
+  "contributors.designer": "Desainer",
+  "contributors.translator": "Penerjemah",
+} as const;
