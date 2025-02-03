1b34f3626c5f72f3e4267aa8dbf09ef9f991769c
Jakob Rössner <jakob.roessner@outlook.de>
2025-02-03 18:36:38 +1100

    fix: format all files

--- packages/api/env.ts
+++ packages/api/env.ts
@@ -1,9 +1,9 @@
-import { createEnv } from '@t3-oss/env-nextjs'
-import { z } from 'zod'
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    NODE_ENV: z.enum(['development', 'test', 'production']),
+    NODE_ENV: z.enum(["development", "test", "production"]),
     OPEN_WEATHER_API_KEY: z.string().min(1),
     UPSTASH_REDIS_REST_URL: z.string().min(1).url(),
     UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
@@ -20,12 +20,15 @@ export const env = createEnv({
     OPEN_WEATHER_API_KEY: process.env.OPEN_WEATHER_API_KEY,
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
-    UPSTASH_RATELIMITER_TOKENS_PER_TIME: process.env.UPSTASH_RATELIMITER_TOKENS_PER_TIME,
-    UPSTASH_RATELIMITER_TIME_INTERVAL: process.env.UPSTASH_RATELIMITER_TIME_INTERVAL,
-    UPSTASH_RATELIMITER_EXCLUDED_IPS: process.env.UPSTASH_RATELIMITER_EXCLUDED_IPS,
+    UPSTASH_RATELIMITER_TOKENS_PER_TIME:
+      process.env.UPSTASH_RATELIMITER_TOKENS_PER_TIME,
+    UPSTASH_RATELIMITER_TIME_INTERVAL:
+      process.env.UPSTASH_RATELIMITER_TIME_INTERVAL,
+    UPSTASH_RATELIMITER_EXCLUDED_IPS:
+      process.env.UPSTASH_RATELIMITER_EXCLUDED_IPS,
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     QWEATHER_API_KEY: process.env.QWEATHER_API_KEY,
-    API_NINJA_API_KEY: process.env.API_NINJA_API_KEY
+    API_NINJA_API_KEY: process.env.API_NINJA_API_KEY,
   },
-  skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION
-})
+  skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
+});
